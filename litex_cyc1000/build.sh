88b9400f7f15da64903bb94681776185253d4e8d
hubmartin <hub.martin@gmail.com>
2026-09-22 10:19:54 +0200

    feat: enable LED chaser in default build

--- litex_cyc1000/build.sh
+++ litex_cyc1000/build.sh
@@ -4,7 +4,7 @@ cd -- "$(dirname -- "$0")"
 source ./litex_env.sh
 export PATH=/home/martin/altera_lite/25.1std/quartus/bin:$PATH
 "$PYTHON" soc.py \
-  --build --with-buttons --no-led-chaser --with-ethernet --eth-ip 192.168.1.241 \
+  --build --with-buttons --with-ethernet --eth-ip 192.168.1.241 \
   --cpu-variant=minimal --integrated-main-ram-size 8192 \
   --with-jtagbone \
   --sys-clk-freq 50e6 --output-dir build
